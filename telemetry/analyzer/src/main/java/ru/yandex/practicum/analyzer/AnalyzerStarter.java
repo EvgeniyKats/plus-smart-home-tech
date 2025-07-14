0452a2acfb4da7988040b6cc78f82cf0cb181758
Evgeniy Kats <eSunr1se@yandex.ru>
2025-07-14 22:57:20 +0500

    add main class, starter and processes template

---
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/AnalyzerStarter.java
@@ -0,0 +1,34 @@
+package ru.yandex.practicum.analyzer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.analyzer.processor.hub.HubEventProcessor;
+import ru.yandex.practicum.analyzer.processor.snapshot.SnapshotProcessor;
+
+/**
+ * Запускает выполнение процессов
+ */
+@Component
+@RequiredArgsConstructor
+public class AnalyzerStarter implements CommandLineRunner {
+    private final HubEventProcessor hubEventProcessor;
+    private final SnapshotProcessor snapshotProcessor;
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Создание потока для обработки hub event
+        Thread hubEventThread = new Thread(hubEventProcessor);
+        hubEventThread.setName("HubEventThread");
+        hubEventThread.start();
+
+        // Создание потока для обработки snapshot
+        Thread snapshotThread = new Thread(snapshotProcessor);
+        snapshotThread.setName("SnapshotThread");
+        snapshotThread.start();
+
+        // Блокировка до завершения работы потоков
+        hubEventThread.join();
+        snapshotThread.join();
+    }
+}
---
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/processor/hub/HubEventProcessor.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.analyzer.processor.hub;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Запускает цикл опроса и обработки событий добавления/удаления устройств и сценариев
+ */
+@Component
+public class HubEventProcessor implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
