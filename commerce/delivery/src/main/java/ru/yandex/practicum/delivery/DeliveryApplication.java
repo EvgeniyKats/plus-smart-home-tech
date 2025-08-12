1cf9546c97042df0fb0cc0752bf44eb446ca5e47
Evgeniy Kats <eSunr1se@yandex.ru>
2025-08-12 09:31:58 +0500

    setup properties-files, pom-files and compose for order, payment, delivery

--- commerce/delivery/src/main/java/ru/yandex/practicum/delivery/DeliveryApplication.java
+++ commerce/delivery/src/main/java/ru/yandex/practicum/delivery/DeliveryApplication.java
@@ -1,7 +1,11 @@
 package ru.yandex.practicum.delivery;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class DeliveryApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(DeliveryApplication.class, args);
     }
 }
