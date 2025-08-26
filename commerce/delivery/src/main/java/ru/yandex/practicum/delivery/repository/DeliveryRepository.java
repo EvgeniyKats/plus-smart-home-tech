23d7801de26e66529a484547211791d812a7262b
Evgeniy Kats <eSunr1se@yandex.ru>
2025-08-26 23:44:22 +0500

    delete unused method DeliveryRepository.findByOrderId()
    + delete unused imports

--- commerce/delivery/src/main/java/ru/yandex/practicum/delivery/repository/DeliveryRepository.java
+++ commerce/delivery/src/main/java/ru/yandex/practicum/delivery/repository/DeliveryRepository.java
@@ -3,9 +3,7 @@ package ru.yandex.practicum.delivery.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.yandex.practicum.delivery.model.Delivery;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface DeliveryRepository extends JpaRepository<Delivery, UUID> {
-    Optional<Delivery> findByOrderId(UUID orderId);
 }
